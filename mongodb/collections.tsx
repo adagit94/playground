cfc70d32cd87881c059be2bb27eb6ddd98469c08
adagit94 <56156969+adagit94@users.noreply.github.com>
2020-03-31 19:36:31 +0200

    c

--- mongodb/collections.tsx
+++
@@ -1,9 +0,0 @@
-import { RemoteMongoClient } from 'mongodb-stitch-browser-sdk';
-
-import { stitchClient } from './stitch-client';
-
-const db = stitchClient
-  .getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas')
-  .db('playground-db');
-
-export const collectionUsers = db.collection('users');
--- mongodb/users.tsx
+++
@@ -1,3 +0,0 @@
-import { users } from './collections';
-
-
