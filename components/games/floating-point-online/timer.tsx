9800c9438744fceb7f1deba11619ad2db88bef8d
adagit94 <56156969+adagit94@users.noreply.github.com>
2020-05-30 16:24:42 +0200

    c

--- components/games/floating-point-online/timer.tsx
+++ components/games/floating-point-online/timer.tsx
@@ -1,4 +1,4 @@
-import { memo, useContext, useEffect } from 'react';
+import { memo, useContext, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
 import { updateDataGame } from 'firebase/db';
@@ -27,24 +27,28 @@ const Timer: React.FC = (): JSX.Element => {
 
   const uid = user?.uid;
 
+  const stateRef = useRef(state);
+
   const handleTimer = (): void => {
-    if (timer === 0) {
-      updateDataGame('floatingPoint', {
-        state: 'eval',
-        timestampEnd: Date.now()
-      });
-    } else {
-      updateDataGame('floatingPoint', { timer: timer - 1 });
+    console.log(stateRef.current);
+    if (stateRef.current === 'run') {
+      if (timer === 0) {
+        updateDataGame('floatingPoint', {
+          state: 'eval',
+          timestampEnd: Date.now()
+        });
+      } else {
+        updateDataGame('floatingPoint', { timer: timer - 1 });
+      }
     }
   };
 
   useEffect(() => {
-    if (
-      state === 'run' &&
-      uid !== undefined &&
-      admin !== undefined &&
-      uid === admin
-    ) {
+    stateRef.current = state;
+  });
+
+  useEffect(() => {
+    if (uid !== undefined && admin !== undefined && uid === admin) {
       window.setTimeout(handleTimer, 1000);
     }
   });
