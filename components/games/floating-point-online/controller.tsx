af266c8b6a748fa42149747c4123deaaf399debb
adagit94 <56156969+adagit94@users.noreply.github.com>
2020-06-06 13:52:21 +0200

    c

--- components/games/floating-point-online/controller.tsx
+++ components/games/floating-point-online/controller.tsx
@@ -171,15 +171,15 @@ const Controller: React.FC = (): JSX.Element => {
             wins: winnerUserData.wins + 1
           });
         } else {
-          await crudDataGame('floatingPoint', 'update', {
+          crudDataGame('floatingPoint', 'update', {
             winner: results
           });
         }
 
-        updatePlayedTime('floatingPoint', players, [
+        /*updatePlayedTime('floatingPoint', players, [
           statesGame.timestampStart,
           statesGame.timestampEnd
-        ]);
+        ]);*/
       };
 
       evalGame();
